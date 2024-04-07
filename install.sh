1b1c10667cc3dd5c4dfe388ad6e42cf3de7b5d05
Roy Seto <royseto@users.noreply.github.com>
2024-04-07 14:23:44 -0700

    Revert "Add install.sh for VS code integration"
    This reverts commit 1e869fc3c5f3a999a82249e9dbec5f98730e0745.

--- install.sh
+++
@@ -1,5 +0,0 @@
-git clone https://github.com/andsens/homeshick.git $HOME/.homesick/repos/homeshick && \
-    source $HOME/.homesick/repos/homeshick/homeshick.sh && \
-    homeshick -b clone https://github.com/royseto/dotfiles.git && \
-    homeshick -f link && \
-    source $HOME/.zshrc
